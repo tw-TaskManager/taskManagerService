6d8ea3fa7b80eaea53e305d830786fec2cfc2c0f
vikashupadhyay <vikasku@thoughtworks.com>
2017-01-02 11:09:32 +0530

    all url changes

--- router/controller.go
+++ router/controller.go
@@ -9,10 +9,10 @@ import (
 
 func HandleRequest(db *sql.DB) {
 	handler := mux.NewRouter()
-	handler.HandleFunc("/tasks/save/{id:[a-z]+}", taskHandler.SaveTask(db)).Methods("POST")
-	handler.HandleFunc("/task/delete/{id:[a-z]+}", taskHandler.DeleteTask(db)).Methods("POST")
-	handler.HandleFunc("/task/update/{id:[a-z]+}", taskHandler.UpdateTask(db)).Methods("POST")
-	handler.HandleFunc("/tasks/{id:[a-z]+}", taskHandler.GetAllTask(db)).Methods("GET")
+	handler.HandleFunc("/tasks/save/{id:\\S+}", taskHandler.SaveTask(db)).Methods("POST")
+	handler.HandleFunc("/task/delete/{id:\\S+}", taskHandler.DeleteTask(db)).Methods("POST")
+	handler.HandleFunc("/task/update/{id:\\S+}", taskHandler.UpdateTask(db)).Methods("POST")
+	handler.HandleFunc("/tasks/{id:\\S+}", taskHandler.GetAllTask(db)).Methods("GET")
 	handler.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
 	http.Handle("/", handler)
 }
