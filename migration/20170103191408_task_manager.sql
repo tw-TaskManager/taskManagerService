c48ccaecdf0bf774f8e13a9aa8919d61cbc55391
vikashupadhyay <vikasku@thoughtworks.com>
2017-01-03 20:27:07 +0530

    user id is reffering to user table

--- migration/20170103191408_task_manager.sql
+++ migration/20170103191408_task_manager.sql
@@ -3,8 +3,9 @@
 CREATE TABLE task_manager(
     "id" serial PRIMARY key NOT NULL,
     "task" text NOT NULL,
-    "userId" text NOT NULL
+    "userId" INT REFERENCES task_manager_user("userId")
 );
+
 
 -- SQL in section 'Up' is executed when this migration is applied
 
